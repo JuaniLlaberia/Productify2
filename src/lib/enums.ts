f29665d773572b48ec17469a0a4c90f4d87d2093
JuaniLlaberia <juanillaberia2002@gmail.com>
2024-10-24 00:11:57 -0300

    Create enums

---
+++ src/lib/enums.ts
@@ -0,0 +1,22 @@
+export enum PriorityEnum {
+  LOW = 'low',
+  MEDIUM = 'medium',
+  HIGH = 'high',
+  URGENT = 'urgent',
+}
+
+export enum StatusEnum {
+  BACKLOG = 'backlog',
+  TODO = 'todo',
+  IN_PROGRESS = 'in-progress',
+  COMPLETED = 'completed',
+  CANCELED = 'canceled',
+}
+
+export enum ReportTypeEnum {
+  INTERFACE = 'interface',
+  FUNCTIONAL = 'functional',
+  PERFORMANCE = 'performance',
+  SECURITY = 'security',
+  OTHER = 'other',
+}
