9922b43c996c07e3473e9b02bd035b969c1fe3be
JuaniLlaberia <juanillaberia2002@gmail.com>
2024-10-24 16:48:41 -0300

    Update labels form

--- src/app/team/[teamId]/projects/[projectId]/labels/(components)/LabelsForm.tsx
+++ src/app/team/[teamId]/projects/[projectId]/labels/(components)/LabelsForm.tsx
@@ -32,23 +32,7 @@ import {
   TooltipTrigger,
 } from '@/components/ui/tooltip';
 import { cn } from '@/lib/utils';
-
-const COLORS = [
-  { label: 'red', value: '#ef4444' },
-  { label: 'salmon', value: '#f87171' },
-  { label: 'orange', value: '#f97316' },
-  { label: 'yellow', value: '#fde047' },
-  { label: 'pink', value: '#f472b6' },
-  { label: 'sky', value: '#38bdf8' },
-  { label: 'blue', value: '#3b82f6' },
-  { label: 'purple', value: '#8b5cf6' },
-  { label: 'fusia', value: '#d946ef' },
-  { label: 'indigo', value: '#818cf8' },
-  { label: 'green', value: '#22c55e' },
-  { label: 'emerald', value: '#34d399' },
-  { label: 'gray', value: '#9ca3af' },
-  { label: 'slate', value: '#94a3b8' },
-];
+import { COLORS } from '@/lib/consts';
 
 const LabelsForm = () => {
   const {
@@ -80,8 +64,6 @@ const LabelsForm = () => {
       error: 'Failed to create label',
     });
   });
-
-  console.log(errors);
 
   return (
     <form onSubmit={submitHandler} className='space-y-5'>
