e767093d0f6603f550444a4b312c1c29c971406c
JuaniLlaberia <juanillaberia2002@gmail.com>
2024-10-23 21:02:48 -0300

    Build extra tasks pages navbar

---
+++ src/app/team/[teamId]/projects/my-tasks/page.tsx
@@ -0,0 +1,54 @@
+import { Columns3, List, Sheet } from 'lucide-react';
+import ProjectsNavbar from '../[projectId]/(components)/Navbar';
+
+const FILTERS = [
+  {
+    label: 'Priority',
+    field: 'priority',
+    options: ['low', 'medium', 'high', 'urgent'],
+  },
+  {
+    label: 'Status',
+    field: 'status',
+    options: ['backlog', 'todo', 'in-progress', 'completed', 'canceled'],
+  },
+  {
+    label: 'Label',
+    field: 'label',
+    options: ['Test', 'Test', 'Test'],
+  },
+];
+
+const MyTasksPage = () => {
+  return (
+    <section className='w-full'>
+      <ProjectsNavbar
+        filters={FILTERS}
+        views={[
+          {
+            id: 'board',
+            label: 'Board',
+            icon: <Columns3 className='size-4' strokeWidth={1.5} />,
+            value: 'board',
+          },
+          {
+            id: 'table',
+            label: 'Table',
+            icon: <Sheet className='size-4' strokeWidth={1.5} />,
+            value: 'table',
+          },
+          {
+            id: 'list',
+            label: 'List',
+            icon: <List className='size-4' strokeWidth={1.5} />,
+            value: 'list',
+          },
+        ]}
+        defaultView='board'
+      />
+      <div>MY TASKS</div>
+    </section>
+  );
+};
+
+export default MyTasksPage;
