38fe06ef8254644817dbcb60059e1e02f4410b30
JuaniLlaberia <juanillaberia2002@gmail.com>
2024-10-26 19:39:25 -0300

    Implement my tasks page

--- src/app/team/[teamId]/projects/my-tasks/page.tsx
+++ src/app/team/[teamId]/projects/my-tasks/page.tsx
@@ -1,5 +1,16 @@
-import { Columns3, List, Sheet } from 'lucide-react';
-import ProjectsNavbar from '../[projectId]/(components)/Navbar';
+'use client';
+
+import { Columns3, Sheet } from 'lucide-react';
+import { useQuery } from 'convex/react';
+
+import ProjectFeatureNavbar from '../[projectId]/(components)/ProjectFeatureNavbar';
+import DeleteTasksModal from '../[projectId]/tasks/(components)/DeleteTasksModal';
+import { api } from '../../../../../../convex/_generated/api';
+import { PriorityEnum, StatusEnum } from '@/lib/enums';
+import { Id } from '../../../../../../convex/_generated/dataModel';
+import { tasksColumns } from '../[projectId]/tasks/(components)/tasksColumns';
+import { DataTable } from '@/components/ui/data-table';
+import { TableProvider } from '@/components/TableContext';
 
 const FILTERS = [
   {
@@ -19,35 +30,59 @@ const FILTERS = [
   },
 ];
 
-const MyTasksPage = () => {
+const VIEWS = [
+  {
+    id: 'board',
+    label: 'Board',
+    icon: <Columns3 className='size-4' strokeWidth={1.5} />,
+    value: 'board',
+  },
+  {
+    id: 'table',
+    label: 'Table',
+    icon: <Sheet className='size-4' strokeWidth={1.5} />,
+    value: 'table',
+  },
+];
+
+const MyTasksPage = ({
+  params: { teamId },
+  searchParams: { priority, status },
+}: {
+  params: {
+    teamId: Id<'teams'>;
+  };
+  searchParams: {
+    priority: PriorityEnum;
+    status: StatusEnum;
+    view: 'board' | 'table';
+  };
+}) => {
+  const tasks = useQuery(api.tasks.getUserTasksInTeam, {
+    teamId,
+    filters: {
+      priority,
+      status,
+    },
+  });
+
+  if (!tasks) return <p>Loading</p>;
+
   return (
-    <section className='w-full'>
-      <ProjectsNavbar
-        filters={FILTERS}
-        views={[
-          {
-            id: 'board',
-            label: 'Board',
-            icon: <Columns3 className='size-4' strokeWidth={1.5} />,
-            value: 'board',
-          },
-          {
-            id: 'table',
-            label: 'Table',
-            icon: <Sheet className='size-4' strokeWidth={1.5} />,
-            value: 'table',
-          },
-          {
-            id: 'list',
-            label: 'List',
-            icon: <List className='size-4' strokeWidth={1.5} />,
-            value: 'list',
-          },
-        ]}
-        defaultView='board'
-      />
-      <div>MY TASKS</div>
-    </section>
+    <TableProvider>
+      <section className='w-full'>
+        <ProjectFeatureNavbar
+          filters={FILTERS}
+          views={VIEWS}
+          defaultView='board'
+        />
+        <DataTable
+          columns={tasksColumns}
+          data={tasks}
+          DeleteModal={DeleteTasksModal}
+        />
+      </section>
+    </TableProvider>
   );
 };
 
