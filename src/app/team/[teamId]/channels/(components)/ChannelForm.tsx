cbfedba2f21bc7a31d5f5e04144e2f2e7146de29
JuaniLlaberia <juanillaberia2002@gmail.com>
2024-11-15 21:10:22 -0300

    Update channel components

--- src/app/team/[teamId]/channels/(components)/ChannelForm.tsx
+++ src/app/team/[teamId]/channels/(components)/ChannelForm.tsx
@@ -9,27 +9,41 @@ import { toast } from 'sonner';
 import { zodResolver } from '@hookform/resolvers/zod';
 
 import InputWrapper from '@/components/ui/input-wrapper';
-import { Id } from '../../../../../../convex/_generated/dataModel';
+import { Doc, Id } from '../../../../../../convex/_generated/dataModel';
 import { ChannelSchema } from '@/lib/validators';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Switch } from '@/components/ui/switch';
 import {
+  Dialog,
   DialogClose,
+  DialogContent,
   DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
+  DialogTrigger,
 } from '@/components/ui/dialog';
 import { Button } from '@/components/ui/button';
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
 import { EmojiPopover } from '@/components/ui/emoji-popover';
+import { type ReactNode, useState } from 'react';
 
-const ChannelForm = () => {
+type ChannelFormProps = {
+  channelData?: Doc<'channels'>;
+  trigger?: ReactNode;
+  onClose?: () => void;
+};
+
+const ChannelForm = ({ channelData, trigger, onClose }: ChannelFormProps) => {
+  const isEditMode = Boolean(channelData?._id);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
   const router = useRouter();
+
   const { teamId } = useParams<{
     teamId: Id<'teams'>;
   }>();
+
   const {
     register,
     handleSubmit,
@@ -39,103 +53,129 @@ const ChannelForm = () => {
     setValue,
   } = useForm({
     defaultValues: {
-      name: '',
-      private: false,
-      icon: '',
+      name: channelData?.name || '',
+      private: channelData?.private || false,
+      icon: channelData?.icon || '',
     },
     resolver: zodResolver(ChannelSchema),
   });
+
   const createChannel = useMutation(api.channels.createChannel);
+  const editChannel = useMutation(api.channels.updateChannel);
 
   const submitHandler = handleSubmit(async data => {
-    const promise = createChannel({
-      teamId,
-      channelData: {
-        name: data.name,
-        private: data.private,
-      },
-    }).then(channelId => {
-      router.push(`/team/${teamId}/channels/${channelId}`);
-    });
+    const channelPayload = {
+      name: data.name,
+      icon: data.icon,
+      private: data.private,
+    };
 
-    toast.promise(promise, {
-      loading: 'Creating new document',
-      success: 'Document created successfully',
-      error: 'Failed to create document',
-    });
+    try {
+      const channelId = await (isEditMode
+        ? editChannel({
+            teamId,
+            channelId: channelData!._id,
+            channelData: channelPayload,
+          })
+        : createChannel({ teamId, channelData: channelPayload }));
+
+      setIsOpen(false);
+      onClose?.();
+
+      router.push(`/team/${teamId}/channels/${channelId}`);
+      toast.success(
+        `Channel ${isEditMode ? 'updated' : 'created'} successfully`
+      );
+    } catch {
+      toast.error(`Failed to ${isEditMode ? 'update' : 'create'} channel`);
+    }
   });
 
   return (
-    <form onSubmit={submitHandler} className='space-y-5'>
-      <DialogHeader>
-        <DialogTitle>Create channel</DialogTitle>
-        <DialogDescription>
-          Channel allows you to communicate between and within teams.
-        </DialogDescription>
-      </DialogHeader>
-      <fieldset className='space-y-4'>
-        <div className='flex items-end gap-1.5'>
-          <InputWrapper
-            label='Name & Icon'
-            error={errors.name?.message as string}
-            inputId='name'
-            className='w-full'
-          >
-            <Input
-              id='name'
-              placeholder='e.g. Front-End, Research, HR'
-              {...register('name')}
-            />
-          </InputWrapper>
-          <EmojiPopover
-            onEmojiSelect={emoji => {
-              setValue('icon', emoji.native);
-            }}
-          >
-            <button className='flex items-center justify-center text-muted-foreground size-10 shrink-0 rounded-lg border border-input bg-transparent cursor-pointer hover:text-primary transition-all'>
-              <span className='sr-only'>Select icon</span>
-              {!watch('icon') ? <Plus className='size-4' /> : watch('icon')}
-            </button>
-          </EmojiPopover>
-        </div>
-        <div className='flex items-center justify-between py-1'>
-          <div className='flex flex-col'>
-            <Label>Make private</Label>
-            <p className='text-sm text-muted-foreground'>
-              Only members will see it
-            </p>
-          </div>
-          <Controller
-            control={control}
-            name='private'
-            render={({ field: { onChange, value } }) => (
-              <Switch onCheckedChange={onChange} checked={value} />
-            )}
-          />
-        </div>
-        <Alert variant='informative'>
-          <AlertCircle className='size-4' />
-          <AlertTitle>Information</AlertTitle>
-          <AlertDescription>
-            Public project will be accessible to all team members
-          </AlertDescription>
-        </Alert>
-      </fieldset>
-      <DialogFooter className='flex justify-end gap-2'>
-        <DialogClose asChild>
-          <Button size='sm' variant='ghost' disabled={isSubmitting}>
-            Cancel
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <DialogTrigger asChild>
+        {trigger || (
+          <Button size='sm'>
+            <Plus className='size-4 mr-1.5' strokeWidth={2} />
+            New channel
           </Button>
-        </DialogClose>
-        <Button size='sm' disabled={isSubmitting} className='min-w-16'>
-          {isSubmitting ? (
-            <Loader2 className='size-4 animate-spin' />
-          ) : (
-            'Create'
-          )}
-        </Button>
-      </DialogFooter>
-    </form>
+        )}
+      </DialogTrigger>
+      <DialogContent>
+        <form onSubmit={submitHandler} className='space-y-5'>
+          <DialogHeader>
+            <DialogTitle>{isEditMode ? 'Edit' : 'Create'} channel</DialogTitle>
+            <DialogDescription>
+              Channel allows you to communicate between and within teams.
+            </DialogDescription>
+          </DialogHeader>
+          <fieldset className='space-y-4'>
+            <div className='flex items-end gap-1.5'>
+              <InputWrapper
+                label='Name & Icon'
+                error={errors.name?.message as string}
+                inputId='name'
+                className='w-full'
+              >
+                <Input
+                  id='name'
+                  placeholder='e.g. Front-End, Research, HR'
+                  {...register('name')}
+                />
+              </InputWrapper>
+              <EmojiPopover
+                onEmojiSelect={emoji => {
+                  setValue('icon', emoji.native);
+                }}
+              >
+                <button className='flex items-center justify-center text-muted-foreground size-10 shrink-0 rounded-lg border border-input bg-transparent cursor-pointer hover:text-primary transition-all'>
+                  <span className='sr-only'>Select icon</span>
+                  {!watch('icon') ? <Plus className='size-4' /> : watch('icon')}
+                </button>
+              </EmojiPopover>
+            </div>
+            <div className='flex items-center justify-between py-1'>
+              <div className='flex flex-col'>
+                <Label>Make private</Label>
+                <p className='text-sm text-muted-foreground'>
+                  Only members will see it
+                </p>
+              </div>
+              <Controller
+                control={control}
+                name='private'
+                render={({ field: { onChange, value } }) => (
+                  <Switch onCheckedChange={onChange} checked={value} />
+                )}
+              />
+            </div>
+            <Alert variant='informative'>
+              <AlertCircle className='size-4' />
+              <AlertTitle>Information</AlertTitle>
+              <AlertDescription>
+                Public project will be accessible to all team members
+              </AlertDescription>
+            </Alert>
+          </fieldset>
+          <DialogFooter className='flex justify-end gap-2'>
+            <DialogClose asChild>
+              <Button size='sm' variant='ghost' disabled={isSubmitting}>
+                Cancel
+              </Button>
+            </DialogClose>
+            <Button size='sm' disabled={isSubmitting} className='min-w-16'>
+              {isSubmitting ? (
+                <Loader2 className='size-4 animate-spin' />
+              ) : isEditMode ? (
+                'Update'
+              ) : (
+                'Create'
+              )}
+            </Button>
+          </DialogFooter>
+        </form>
+      </DialogContent>
+    </Dialog>
   );
 };
 
--- src/app/team/[teamId]/channels/(components)/ChannelsLinks.tsx
+++ src/app/team/[teamId]/channels/(components)/ChannelsLinks.tsx
@@ -1,13 +1,20 @@
 'use client';
 
-import { Bookmark, PlusCircle, Hash, Plus } from 'lucide-react';
+import {
+  Bookmark,
+  PlusCircle,
+  Hash,
+  Plus,
+  MessageSquareText,
+  Search,
+} from 'lucide-react';
 import { useParams, usePathname } from 'next/navigation';
 import { useQuery } from 'convex/react';
 
 import SidebarLoader from '../../(components)/SidebarLoader';
+import ChannelActions from './ChannelActions';
 import InnerSidebarLink from '../../(components)/InnerSidebarLinks';
 import ChannelForm from './ChannelForm';
-import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog';
 import { api } from '../../../../../../convex/_generated/api';
 import { Id } from '../../../../../../convex/_generated/dataModel';
 
@@ -21,33 +28,51 @@ const ChannelsLinks = () => {
   if (!channels) return <SidebarLoader />;
 
   return (
-    <Dialog>
+    <>
       <h3 className='flex items-center justify-between text-xs uppercase font-semibold text-muted-foreground mb-2'>
         <span className='py-0.5'>General</span>
       </h3>
       <ul className='flex flex-col gap-0.5 mb-4'>
         <li>
-          <DialogTrigger asChild>
-            <button className='flex w-full items-center gap-2 px-2 py-1.5 rounded-lg text-sm hover:bg-muted/40'>
-              <PlusCircle className='size-4 mr-1.5' strokeWidth={1.5} />
-              New channel
-            </button>
-          </DialogTrigger>
+          <ChannelForm
+            trigger={
+              <button className='flex w-full items-center gap-2 px-2 py-1.5 rounded-lg text-sm hover:bg-muted/60'>
+                <PlusCircle className='size-4 mr-1.5' strokeWidth={1.5} />
+                New channel
+              </button>
+            }
+          />
         </li>
         <InnerSidebarLink
-          label='Saved chats'
+          label='Threads'
+          icon={
+            <MessageSquareText className='size-4 mr-1.5' strokeWidth={1.5} />
+          }
+          link={`/team/${teamId}/channels/threads`}
+          isActive={pathname.includes('/channels/threads')}
+        />
+        <InnerSidebarLink
+          label='Saved'
           icon={<Bookmark className='size-4 mr-1.5' strokeWidth={1.5} />}
           link={`/team/${teamId}/channels/saved`}
           isActive={pathname.includes('/channels/saved')}
         />
+        <InnerSidebarLink
+          label='Browse'
+          icon={<Search className='size-4 mr-1.5' strokeWidth={1.5} />}
+          link={`/team/${teamId}/channels/browse`}
+          isActive={pathname.includes('/channels/browse')}
+        />
       </ul>
       <h3 className='flex items-center justify-between text-xs uppercase font-semibold text-muted-foreground mb-2 group'>
         <span className='py-0.5'>Your channels</span>
-        <DialogTrigger asChild>
-          <span className='hover:bg-muted/40 hidden group-hover:flex p-0.5 rounded transition-colors cursor-pointer'>
-            <Plus className='size-4' />
-          </span>
-        </DialogTrigger>
+        <ChannelForm
+          trigger={
+            <span className='hover:bg-muted/40 hidden group-hover:flex p-0.5 rounded transition-colors cursor-pointer'>
+              <Plus className='size-4' />
+            </span>
+          }
+        />
       </h3>
       <ul className='flex flex-col gap-0.5 mb-4'>
         <InnerSidebarLink
@@ -56,26 +81,28 @@ const ChannelsLinks = () => {
           link={`/team/${teamId}/channels/general`}
           isActive={pathname.includes('/channels/general')}
         />
-        {channels.map(channel => (
-          <InnerSidebarLink
-            key={channel?._id}
-            label={channel?.name as string}
-            icon={
-              channel?.icon ? (
-                channel.icon
-              ) : (
-                <Hash className='size-4 mr-1.5' strokeWidth={1.5} />
-              )
-            }
-            link={`/team/${teamId}/channels/${channel?._id}`}
-            isActive={pathname.includes(channel?._id as string)}
-          />
-        ))}
+        {channels.length > 0
+          ? channels.map(channel => (
+              <InnerSidebarLink
+                key={channel?._id}
+                label={channel?.name as string}
+                icon={
+                  channel?.icon ? (
+                    <span className='size-4 mr-1.5'>{channel.icon}</span>
+                  ) : (
+                    <Hash className='size-4 mr-1.5' strokeWidth={1.5} />
+                  )
+                }
+                link={`/team/${teamId}/channels/${channel?._id}`}
+                isActive={pathname.includes(channel?._id as string)}
+                options={
+                  channel ? <ChannelActions data={channel} /> : undefined
+                }
+              />
+            ))
+          : null}
       </ul>
-      <DialogContent>
-        <ChannelForm />
-      </DialogContent>
-    </Dialog>
+    </>
   );
 };
 
