4e57139b90d56aeb3048bdc9724c4e7e9843bc3c
JuaniLlaberia <juanillaberia2002@gmail.com>
2024-11-18 14:19:22 -0300

    Update channel header

--- src/app/team/[teamId]/channels/(components)/ChannelHeader.tsx
+++ src/app/team/[teamId]/channels/(components)/ChannelHeader.tsx
@@ -1,36 +1,21 @@
-'use client';
-
-import { useQuery } from 'convex/react';
 import { Hash } from 'lucide-react';
-import { useParams } from 'next/navigation';
 
-import { api } from '../../../../../../convex/_generated/api';
-import { Id } from '../../../../../../convex/_generated/dataModel';
-import { Skeleton } from '@/components/ui/skeleton';
+type ChannelHeaderProps = {
+  channelName?: string;
+  channelIcon?: string;
+};
 
-const ChannelHeader = () => {
-  const { teamId, channelId } = useParams<{
-    teamId: Id<'teams'>;
-    channelId: Id<'channels'>;
-  }>();
-  const channel = useQuery(api.channels.getChannelById, { teamId, channelId });
-
+const ChannelHeader = ({ channelIcon, channelName }: ChannelHeaderProps) => {
   return (
     <nav className='flex items-center gap-2.5 w-full p-2 px-4 border-b border-border'>
-      {channel ? (
-        <>
-          <div className='p-1 rounded bg-muted text-muted-foreground'>
-            {channel.icon ? (
-              channel.icon
-            ) : (
-              <Hash className='size-4' strokeWidth={1.5} />
-            )}
-          </div>
-          <h1 className='text-sm font-medium'>{channel.name}</h1>
-        </>
-      ) : (
-        <Skeleton className='h-8 w-32' />
-      )}
+      <div className='p-1 rounded bg-muted text-muted-foreground'>
+        {channelIcon ? (
+          channelIcon
+        ) : (
+          <Hash className='size-4' strokeWidth={1.5} />
+        )}
+      </div>
+      <h1 className='text-sm font-medium'>{channelName}</h1>
     </nav>
   );
 };
