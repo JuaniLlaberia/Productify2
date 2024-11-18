2638684e62b0f80968e8b74137a3817fafd0f228
JuaniLlaberia <juanillaberia2002@gmail.com>
2024-11-17 21:32:27 -0300

    Implement custom hook to get paginated messages from convex

---
+++ src/features/messages/api/useGetMessages.ts
@@ -0,0 +1,31 @@
+import { usePaginatedQuery } from 'convex/react';
+
+import { api } from '../../../../convex/_generated/api';
+import { Id } from '../../../../convex/_generated/dataModel';
+
+const BATCH_SIZE = 20;
+
+type UseGetMessagesProps = {
+  teamId: Id<'teams'>;
+  channelId?: Id<'channels'>;
+  conversationId?: Id<'conversations'>;
+  parentMessageId?: Id<'messages'>;
+};
+
+export type GetMessagesReturnType =
+  (typeof api.messages.getMessages._returnType)['page'];
+
+export const useGetMessages = ({
+  teamId,
+  channelId,
+  conversationId,
+  parentMessageId,
+}: UseGetMessagesProps) => {
+  const { results, status, loadMore } = usePaginatedQuery(
+    api.messages.getMessages,
+    { teamId, channelId, conversationId, parentMessageId },
+    { initialNumItems: BATCH_SIZE }
+  );
+
+  return { results, status, loadMore: () => loadMore(BATCH_SIZE) };
+};
