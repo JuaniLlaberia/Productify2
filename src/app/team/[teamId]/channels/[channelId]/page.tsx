b4d25198d1974a66b4f7b5abc8d03d506405916a
JuaniLlaberia <juanillaberia2002@gmail.com>
2024-11-18 14:17:34 -0300

    Added channel data to page

--- src/app/team/[teamId]/channels/[channelId]/page.tsx
+++ src/app/team/[teamId]/channels/[channelId]/page.tsx
@@ -1,13 +1,32 @@
+'use client';
+
+import { useQuery } from 'convex/react';
+
 import ChatInput from '../(components)/ChannelInput';
 import ChannelHeader from '../(components)/ChannelHeader';
 import MessagesList from '../(components)/MessagesList';
+import { api } from '../../../../../../convex/_generated/api';
+import { Id } from '../../../../../../convex/_generated/dataModel';
 
-const ChannelPage = () => {
+const ChannelPage = ({
+  params: { teamId, channelId },
+}: {
+  params: {
+    teamId: Id<'teams'>;
+    channelId: Id<'channels'>;
+  };
+}) => {
+  const channel = useQuery(api.channels.getChannelById, { teamId, channelId });
+  if (!channel) return <p>LOADING</p>;
+
   return (
     <section className='w-full h-full flex flex-col'>
-      <ChannelHeader />
-      <MessagesList />
-      <ChatInput placeholder='Write your message' />
+      <ChannelHeader channelName={channel.name} channelIcon={channel?.icon} />
+      <MessagesList
+        channelName={channel.name}
+        channelCreationTime={channel._creationTime}
+      />
+      <ChatInput placeholder={`Send message to # ${channel.name}`} />
     </section>
   );
 };
