f607bef658a635c13e72be78061a0748d6564414
JuaniLlaberia <juanillaberia2002@gmail.com>
2024-12-05 22:10:05 -0300

    Implement channel separate component

---
+++ src/app/team/[teamId]/chats/channels/(components)/Channel.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { useQuery } from 'convex/react';
+
+import ChatInput from '../../(components)/ChatInput';
+import ChatHeader from '../../(components)/ChatHeader';
+import MessagesList from '../../(components)/MessagesList';
+import { api } from '../../../../../../../convex/_generated/api';
+import { Id } from '../../../../../../../convex/_generated/dataModel';
+import ChannelHeaderMenu from './ChannelHeaderMenu';
+
+type ChannelProps = {
+  teamId: Id<'teams'>;
+  channelId: Id<'channels'>;
+  onClose?: () => void;
+};
+
+const Channel = ({ teamId, channelId, onClose }: ChannelProps) => {
+  const channel = useQuery(api.channels.getChannelById, { teamId, channelId });
+
+  return (
+    <section className='w-full h-screen flex flex-col'>
+      <ChatHeader
+        chatName={channel?.name}
+        chatIcon={channel?.icon}
+        menu={<ChannelHeaderMenu channelId={channelId} />}
+        onClose={onClose}
+      />
+      <MessagesList
+        channelId={channelId}
+        channelName={channel?.name}
+        channelCreationTime={channel?._creationTime}
+      />
+      <ChatInput
+        placeholder={`Send message to # ${channel?.name || 'Channel'}`}
+      />
+    </section>
+  );
+};
+
+export default Channel;
