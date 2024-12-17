940a75c7a047c086032f68c9ee4feaa225291186
JuaniLlaberia <juanillaberia2002@gmail.com>
2024-12-17 17:09:55 -0300

    Implement storages pages

---
+++ src/app/team/[teamId]/storages/[storageId]/page.tsx
@@ -0,0 +1,79 @@
+'use client';
+
+import { CloudUpload, HardDrive } from 'lucide-react';
+import { useQuery } from 'convex/react';
+
+import Header from '@/components/Header';
+import SearchbarFilter from '../../projects/[projectId]/(components)/SearchbarFilter';
+import UploadAssetModal from '../(components)/UploadAssetModal';
+import { useStablePaginatedQuery } from '@/hooks/useStablePaginatedQuery';
+import { Id } from '../../../../../../convex/_generated/dataModel';
+import { api } from '../../../../../../convex/_generated/api';
+import {
+  ColumnVisibilityDropdown,
+  TableProvider,
+} from '@/components/TableContext';
+import { DataTable } from '@/components/ui/data-table';
+import { assetsColumns } from './(components)/assetsColumns';
+import { Button } from '@/components/ui/button';
+import { Skeleton } from '@/components/ui/skeleton';
+
+const StoragePage = ({
+  params: { teamId, storageId },
+}: {
+  params: { teamId: Id<'teams'>; storageId: Id<'storages'> };
+}) => {
+  const storage = useQuery(api.storages.getStorageById, {
+    teamId,
+    storageId,
+  });
+  const { results, isLoading } = useStablePaginatedQuery(
+    api.assets.getAssets,
+    { teamId, storageId },
+    { initialNumItems: 10 }
+  );
+
+  return (
+    <TableProvider>
+      <Header
+        leftContent={
+          <div className='flex items-center gap-2.5'>
+            {storage?.icon ? (
+              storage.icon
+            ) : (
+              <HardDrive className='size-4' strokeWidth={1.5} />
+            )}
+            {storage?.name ? (
+              <h1 className='text-sm font-medium'>{storage.name}</h1>
+            ) : (
+              <Skeleton className='h-7 w-32' />
+            )}
+          </div>
+        }
+      />
+      <Header
+        leftContent={
+          <>
+            <ColumnVisibilityDropdown />
+            <SearchbarFilter field='name' />
+          </>
+        }
+        rightContent={
+          <>
+            <UploadAssetModal
+              trigger={
+                <Button size='sm'>
+                  <CloudUpload className='size-4 mr-1.5' />
+                  Upload asset
+                </Button>
+              }
+            />
+          </>
+        }
+      />
+      <DataTable data={results} isLoading={isLoading} columns={assetsColumns} />
+    </TableProvider>
+  );
+};
+
+export default StoragePage;
---
+++ src/app/team/[teamId]/storages/page.tsx
@@ -0,0 +1,69 @@
+'use client';
+
+import { HardDrive, Plus } from 'lucide-react';
+
+import Header from '@/components/Header';
+import SearchbarFilter from '../projects/[projectId]/(components)/SearchbarFilter';
+import StorageForm from './(components)/StorageForm';
+import {
+  ColumnVisibilityDropdown,
+  TableProvider,
+} from '@/components/TableContext';
+import { useStablePaginatedQuery } from '@/hooks/useStablePaginatedQuery';
+import { api } from '../../../../../convex/_generated/api';
+import { Id } from '../../../../../convex/_generated/dataModel';
+import { DataTable } from '@/components/ui/data-table';
+import { storagesColumns } from './(components)/storagesColumns';
+import { Separator } from '@/components/ui/separator';
+import { Button } from '@/components/ui/button';
+
+const AssetsPage = ({
+  params: { teamId },
+}: {
+  params: { teamId: Id<'teams'> };
+}) => {
+  const { results, isLoading } = useStablePaginatedQuery(
+    api.storages.getAllStorages,
+    { teamId },
+    { initialNumItems: 10 }
+  );
+
+  return (
+    <TableProvider>
+      <Header
+        leftContent={
+          <>
+            <HardDrive className='size-4' strokeWidth={1.5} />
+
+            <h1 className='text-sm font-medium'>Storages</h1>
+          </>
+        }
+        rightContent={
+          <>
+            <ColumnVisibilityDropdown />
+            <SearchbarFilter field='name' />
+            <Separator orientation='vertical' />
+            <StorageForm
+              trigger={
+                <Button size='sm'>
+                  <Plus className='size-4 mr-1.5' strokeWidth={1.5} /> New
+                  Storage
+                </Button>
+              }
+            />
+          </>
+        }
+      />
+      <DataTable
+        data={results}
+        isLoading={isLoading}
+        columns={storagesColumns}
+      />
+      <div className='bg-muted/20 p-1 px-4 border-b flex items-center'>
+        <p className='text-sm text-muted-foreground'>Total: {results.length}</p>
+      </div>
+    </TableProvider>
+  );
+};
+
+export default AssetsPage;
