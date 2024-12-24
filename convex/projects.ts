fc9e6711e95c0fe72c3844510cf5202c6dc0d066
JuaniLlaberia <juanillaberia2002@gmail.com>
2024-12-23 21:48:30 -0300

    Update projects convex functions

--- convex/projects.ts
+++ convex/projects.ts
@@ -4,6 +4,8 @@ import { mutation, query } from './_generated/server';
 import { isAdmin, isMember } from './auth';
 import { Projects } from './schema';
 
+//Projects functions
+
 export const getProjects = query({
   args: { teamId: v.id('teams') },
   handler: async (ctx, args) => {
@@ -50,24 +52,6 @@ export const getProjectById = query({
       .first();
 
     return project;
-  },
-});
-
-export const getProjectMembers = query({
-  args: { teamId: v.id('teams'), projectId: v.id('projects') },
-  handler: async (ctx, args) => {
-    await isMember(ctx, args.teamId);
-
-    const projectMembers = await ctx.db
-      .query('projectMembers')
-      .withIndex('by_projectId', q => q.eq('projectId', args.projectId))
-      .collect();
-    const projectMembersIds = projectMembers.map(member => member.userId);
-    const membersData = await Promise.all(
-      projectMembersIds.map(member => ctx.db.get(member))
-    );
-
-    return membersData;
   },
 });
 
@@ -152,32 +136,84 @@ export const deleteProject = mutation({
   },
 });
 
+//Project members funcitons
+
+export const getProjectMembers = query({
+  args: { teamId: v.id('teams'), projectId: v.id('projects') },
+  handler: async (ctx, args) => {
+    await isMember(ctx, args.teamId);
+
+    const members = await ctx.db
+      .query('projectMembers')
+      .withIndex('by_projectId', q => q.eq('projectId', args.projectId))
+      .collect();
+
+    const membersWithData = await Promise.all(
+      members.map(async member => {
+        const userData = await ctx.db.get(member.userId);
+
+        if (!userData) return null;
+
+        return {
+          ...userData,
+          memberId: member._id,
+          teamId: member.teamId,
+        };
+      })
+    );
+
+    return membersWithData.filter(
+      (member): member is NonNullable<typeof member> => member !== null
+    );
+  },
+});
+
 export const createProjectMember = mutation({
   args: {
     projectId: v.id('projects'),
-    userId: v.id('users'),
+    userIds: v.array(v.id('users')),
     teamId: v.id('teams'),
   },
   handler: async (ctx, args) => {
+    const { teamId, projectId, userIds } = args;
     const user = await isAdmin(ctx, args.teamId);
     if (!user?.role)
       throw new ConvexError(
         'User does not have permissions to perform this action.'
       );
 
-    await ctx.db.insert('projectMembers', {
-      ...args,
-    });
+    const existingMembers = await ctx.db
+      .query('projectMembers')
+      .withIndex('by_projectId', q => q.eq('projectId', projectId))
+      .collect();
+
+    const newUserIds = userIds.filter(
+      userId => !existingMembers.some(member => member.userId === userId)
+    );
+
+    if (newUserIds.length === 0) {
+      throw new ConvexError('All selected users are already project members');
+    }
+
+    await Promise.all(
+      newUserIds.map(user =>
+        ctx.db.insert('projectMembers', {
+          teamId,
+          projectId,
+          userId: user,
+        })
+      )
+    );
   },
 });
 
 export const removeProjectMember = mutation({
   args: {
-    projectMemberId: v.id('projectMembers'),
+    projectMember: v.id('projectMembers'),
     teamId: v.id('teams'),
   },
   handler: async (ctx, args) => {
-    const { projectMemberId, teamId } = args;
+    const { projectMember, teamId } = args;
 
     const user = await isAdmin(ctx, teamId);
     if (!user?.role)
@@ -185,7 +221,7 @@ export const removeProjectMember = mutation({
         'User does not have permissions to perform this action.'
       );
 
-    await ctx.db.delete(projectMemberId);
+    await ctx.db.delete(projectMember);
   },
 });
 
